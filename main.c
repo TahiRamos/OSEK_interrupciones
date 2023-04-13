3114e447f91941826af7860734acbbb0a534e4df
Hegui Tahiri Ramos Ugalde <ie721194@iteso.mx>
2023-04-12 23:04:01 -0600

    compila sin interrupciones

--- main.c
+++ main.c
@@ -19,26 +19,27 @@ Task_struct_t task_list[3];
 int main (void)
 {
 	//TASK A
-	task_list[0].priority = 0;
-	task_list[0].schedule = FULL;
-	task_list[0].autostart = TRUE;
-	task_list[0].state = SUSPENDED;
+	task_list[0].PRIORITY = 0;
+	task_list[0].SCHEDULE = FULL;
+	task_list[0].AUTOSTART = TRUE;
+	task_list[0].ACTIVATION = SUSPENDED;
 	task_list[0].ptr_funct = task_A;
 
 	//TASK B
-	task_list[1].priority = 1;
-	task_list[1].schedule = FULL;
-	task_list[1].autostart = FALSE;
-	task_list[1].state = SUSPENDED;
+	task_list[1].PRIORITY = 1;
+	task_list[1].SCHEDULE = FULL;
+	task_list[1].AUTOSTART = FALSE;
+	task_list[1].ACTIVATION = SUSPENDED;
 	task_list[1].ptr_funct = task_B;
 
 
 	//TASK C
-	task_list[2].priority = 2;
-	task_list[2].schedule = FULL;
-	task_list[2].autostart = FALSE;
-	task_list[2].state = SUSPENDED;
+	task_list[2].PRIORITY = 2;
+	task_list[2].SCHEDULE = FULL;
+	task_list[2].AUTOSTART = FALSE;
+	task_list[2].ACTIVATION = SUSPENDED;
 	task_list[2].ptr_funct = task_C;
+
 
 	NVIC_set_basepri_threshold(PRIORITY_10);
 	/**Enables and sets a particular interrupt and its priority*/
